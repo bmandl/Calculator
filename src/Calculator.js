d742134228e5b4ead0d94580b56197d431a44a4a
ESSTopla <ESSTopla@tab.pri>
2019-07-21 21:29:40 +0200

    some updates

--- src/Calculator.js
+++ src/Calculator.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 
 import './Calculator.scss';
 
+const isOperator = /[x/+-]/,
+    endsWithOperator = /[x+/-]$/,
+    endsWithNumber = /[0-9]$/;
+
 const buttons = [
     {
         id: 'zero',
@@ -95,9 +99,9 @@ class Display extends React.Component {
 
     render() {
         return (
-            <div id="display">
-                <div className="formula-screen">{this.props.formula}</div>
-                <div className="value-screen">{this.props.value}</div>
+            <div>
+                <div id="formula" className="formula-screen">{this.props.formula}</div>
+                <div id="display" className="value-screen">{this.props.value}</div>
             </div>
         );
     }
@@ -142,43 +146,126 @@ class App extends React.Component {
         this.handleNumbers = this.handleNumbers.bind(this);
         this.handleMath = this.handleMath.bind(this);
         this.handleUtils = this.handleUtils.bind(this);
-        this.initialize = this.initialize.bind(this);
+        this.clear = this.clear.bind(this);
         this.evaluate = this.evaluate.bind(this);
+        this.decimal = this.decimal.bind(this);
     }
 
     handleNumbers(e) {
         const btn = e.target.value;
+        if (btn != '.') {
+            if (this.state.evaluated === true) {
+                this.setState(
+                    {
+                        displayValue: btn,
+                        formulaDisplay: btn != '0' ? btn : '',
+                        evaluated: false
+                    }
+                );
+            }
+
+            else {
+                this.setState(state => (
+                    {
+                        displayValue:
+                            state.displayValue == '0' || isOperator.test(state.displayValue) ? btn : state.displayValue + btn,
+                        formulaDisplay:
+                            state.displayValue == '0' && btn == '0' ? state.formulaDisplay :
+                                /([^.0-9]0)$/.test(state.formulaDisplay) ? state.formulaDisplay.slice(0, -1) + btn : state.formulaDisplay + btn
+                    }
+                )
+                );
+            }
+        }
+        else this.decimal();
+    }
+
+    decimal() {
         if (this.state.evaluated === true) {
             this.setState(
                 {
-                    displayValue: btn,
-                    formulaDisplay: btn != '0' ? btn : ''
+                    displayValue: '0.',
+                    formulaDisplay: '0.',
+                    evaluated: false
                 }
-            );
+            )
         }
-
         else {
             this.setState(state => (
-                { displayValue: state.displayValue == '0' ? btn : state.displayValue + btn }
-            )
-            );
+                {
+                    displayValue:
+                        state.displayValue.includes('.') ? state.displayValue : isOperator.test(state.displayValue) ? '0.' : state.displayValue + '.',
+                    formulaDisplay:
+                        endsWithOperator.test(state.formulaDisplay) || state.formulaDisplay === '-' ?
+                            state.formulaDisplay + '0.' : /[.]$/.test(state.formulaDisplay) || state.displayValue.includes('.') ?
+                                state.formulaDisplay : state.formulaDisplay === '' ? '0.' : state.formulaDisplay + '.',
+                    evaluated: false
+                }
+            ));
         }
     }
 
-    handleMath(btn) {
+    handleMath(e) {
+        let btn = e.target.value;
+        if (this.state.evaluated === true) {
+            this.setState(state => (
+                {
+                    displayValue: btn,
+                    formulaDisplay: state.displayValue + btn,
+                    evaluated: false
+                }
+            ));
+        }
 
+        this.setState(state => (
+            {
+                displayValue:
+                    endsWithNumber.test(state.displayValue) || isOperator.test(state.displayValue) ? btn : state.displayValue,
+                formulaDisplay:
+                    state.formulaDisplay === '' && btn != '-' ? 
+                    '0' + btn : endsWithNumber.test(state.formulaDisplay) /*|| (btn === '-' && /[x/]$/.test(state.formulaDisplay))*/ ? 
+                    state.formulaDisplay + btn : endsWithOperator.test(state.formulaDisplay) ? 
+                    state.formulaDisplay.slice(0, -1) + btn : state.formulaDisplay
+            }
+        )
+        );
     }
 
-    handleUtils(btn) {
+    handleUtils(e) {
+        let btn = e.target.value;
 
+        switch (btn) {
+            case '=':
+                this.evaluate();
+                break;
+            case 'C':
+                this.clear()
+        }
     }
 
-    initialize() {
-
+    clear() {
+        this.setState(
+            {
+                displayValue: '0',
+                formulaDisplay: '',
+                evaluated: false
+            }
+        );
     }
 
     evaluate() {
+        let expression = endsWithOperator.test(this.state.formulaDisplay) || /[.]$/.test(this.state.formulaDisplay) ? 
+        this.state.formulaDisplay.slice(0, -1) : this.state.evaluated === true ? this.state.displayValue : this.state.formulaDisplay;
+        expression = expression.replace(/x/g,'*');
+        let result = eval(expression);
 
+        this.setState(state => (
+            {
+                displayValue: result.toString(),
+                formulaDisplay: '',
+                evaluated: true
+            }
+        ));
     }
 
     render() {
